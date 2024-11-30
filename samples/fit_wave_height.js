c44d20faeec043909a5783d444902bf5b2c457c6
DESKTOP-EO3O4U9\Otarie <adrien.tocqueville@unity3d.com>
2024-11-30 17:04:59 +0100

    wave height sample

---
+++ samples/fit_wave_height.js
@@ -0,0 +1,92 @@
+
+let table = (() => {
+	
+	let table_str = `x	0.001000	3.634222	7.267445	10.900667	14.533890	18.167112	21.800335	25.433556	29.066778	32.700001
+0.500000	0.000000	0.172189	0.349069	0.502313	0.591989	0.685910	0.790743	0.876951	1.110142	1.076240
+28.222223	0.000219	1.729689	3.117172	4.289617	5.568840	6.842489	7.517123	7.769580	8.713974	10.185813
+55.944447	0.000921	2.454033	4.366206	5.713319	7.005017	8.196212	10.572537	11.603674	13.054770	14.761792
+83.666672	0.001308	3.117125	4.856600	6.958553	8.878926	10.098042	11.017612	12.129695	14.762961	16.196167
+111.388893	0.001714	3.459834	5.745524	7.601034	9.358914	11.492421	12.777795	15.239424	17.194862	19.639803
+139.111115	0.001764	3.324723	6.164509	8.666711	9.865814	13.719121	15.663674	17.064583	19.782461	21.893198
+166.833344	0.002014	4.163740	6.634459	8.956992	11.008577	14.281808	17.740055	20.627918	22.348070	23.988289
+194.555557	0.002345	4.541253	7.452064	11.590300	15.218862	16.677652	17.272060	19.654284	22.630735	27.205545
+222.277786	0.002503	4.834609	8.117563	10.493383	13.833505	16.880219	18.714115	20.068998	24.091259	26.402630
+250.000015	0.002436	4.852196	9.659721	12.062520	14.511833	17.701851	19.803167	22.160175	25.340780	29.412657`;
+
+	let lines = table_str.split("\n");
+	let sample_count = 10;
+	
+	let result = {
+		axis_w: [],
+		axis_f: [],
+		data: [] // index fetch then wind
+	}
+	
+	result.axis_w.push(0) // Force wind 0 to be at 0
+	
+	let speeds = lines[0].split("\t");
+	for (let wind = 0; wind < sample_count; wind++)
+		result.axis_w.push(parseFloat(speeds[wind + 1]))
+
+	for (let fetch = 0; fetch < sample_count; fetch++)
+	{
+		let values = lines[fetch + 1].split("\t");
+		result.axis_f.push(parseFloat(values[0]))
+		result.data.push([0])
+		for (let wind = 0; wind < sample_count; wind++)
+		{
+			result.data[fetch].push(parseFloat(values[wind + 1]))
+		}
+	}
+	
+	return result;
+})();
+
+register_sample("Wave height", () => {
+
+    function water_height_table(fetch, windSpeed)
+    {
+		function find_uv(value, axis) {
+			let i0 = axis.length-1;
+			for (let i = 0; i < axis.length; i++)
+			{
+				if (axis[i] > value)
+				{
+					i0 = max(0, i - 1);
+					break;
+				}
+			}
+			let i1 = min(i0 + 1, axis.length - 1);
+			
+			let v0 = axis[i0], v1 = axis[i1];
+			let t = saturate((value - v0) / (v1 - v0));
+			
+			return [i0, i1, t];
+		}
+		
+		let f = find_uv(fetch, table.axis_f);
+		let w = find_uv(windSpeed, table.axis_w);
+		
+		let low = lerp(table.data[f[0]][w[0]], table.data[f[1]][w[0]], f[2]);
+		let high = lerp(table.data[f[0]][w[1]], table.data[f[1]][w[1]], f[2]);
+		
+		return lerp(low, high, w[2]);
+    }
+	
+    Variable.get("fetch", {min: 0.5, max: 250, res: 32});
+    Variable.get("windSpeed", {min: 0, max: 32.7, res: 32});
+	
+    new Expression(water_height_table);
+	
+    new Fitting({
+        ref: water_height_table,
+        value: "polynom(windSpeed, polynom(fetch, a1, b1),  polynom(fetch, a2, b2), 0)",
+    }, "water_height_fit");
+	
+    new Plot({
+		axis_1: "fetch",
+		axis_2: "windSpeed",
+		
+        functions: [water_height_table, "water_height_fit"]
+    });
+});
--- utils/math_lib.js
+++ utils/math_lib.js
@@ -1,3 +1,4 @@
+
 function element_wise(action, vector_args, scalar_args)
 {
 	if (scalar_args == undefined) scalar_args = [];
@@ -70,12 +71,11 @@ function float4(x, y, z, w) { return new Proxy([x || 0, y || 0, z || 0, w || 0],
 function random(min = 0, max = 1) { return Math.random() * (max - min) + min; }
 function truncate(x, precision=2) { return Number(x.toFixed(precision)); }
 
-//TODO: reverse
 function polynom(x)
 {
-    let res = arguments[1];
+    let res = arguments[arguments.length-1];
     let x_p = x;
-    for (let i = 2; i < arguments.length; i++)
+    for (let i = arguments.length - 2; i >= 1; i--)
     {
         res += x_p * arguments[i];
         x_p *= x;
@@ -90,4 +90,33 @@ function squared_error(model, dataset, params)
 		for (let i = 0; i < n; i++)
 				error += Math.pow(model(...x[i], ...params) - y[i], 2);
 		return error;
+}
+
+// array2d is indexed by y then by x, cause when you print in the console it's this order
+// doesn't support wrapping
+function bilinear_sample(array2d, uv)
+{
+	let size_y = array2d.length;
+	let size_x = array2d[0].length;
+	
+    x = saturate(uv[0]) * (size_x - 1);
+    y = saturate(uv[1]) * (size_y - 1);
+
+    let x_low = floor(x);
+    let y_low = floor(y);
+    let x_lerp = (x - x_low);
+
+    let low = array2d[min(y_low, size_y-1)][min(x_low, size_x-1)];
+    if (x_low+1 < size_x)
+        low = lerp(low, array2d[min(y_low, size_y-1)][x_low+1], x_lerp);
+
+    let high = low;
+    if (y_low+1 < size_y)
+    {
+        high = array2d[y_low+1][min(x_low, size_x-1)];
+		if (x_low+1 < size_x)
+			high = lerp(high, array2d[y_low+1][x_low+1], x_lerp);
+    }
+
+    return lerp(low, high, y - y_low);
 }
